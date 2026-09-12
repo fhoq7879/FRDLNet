bcbbcedca2e826cb3c56edeff6d477e20ceb748c
fhoq7879 <fhoq2002@163.com>
2026-09-12 21:06:15 +0800

    Create meta_template.py

---
+++ meta_template.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+from abc import abstractmethod
+
+class MetaTemplate(nn.Module):
+    def __init__(self, model_func, n_way, n_support, change_way = True):
+        super(MetaTemplate, self).__init__()
+        # 添加设备属性
+        self.device = torch.device("cpu")  # 默认使用CPU
+        self.n_way      = n_way
+        self.n_support  = n_support
+        self.n_query    = -1 #(change depends on input) 
+        self.feature    = model_func
+        self.feat_dim   = self.feature.final_feat_dim
+        self.change_way = change_way  #some methods allow different_way classification during training and test
+
+    @abstractmethod
+    def set_forward(self,x,is_feature):
+        pass
+
+    @abstractmethod
+    def set_forward_loss(self, x):
+        pass
+
+    def forward(self,x):
+        out  = self.feature.forward(x)
+        return out
+
+    def parse_feature(self,x,is_feature):
+        # 使用设备变量，而不是硬编码的cuda()
+        if hasattr(self, 'device'):
+            x = Variable(x.to(self.device))
+        else:
+            x = Variable(x.to('cpu'))  # 默认使用CPU
+        if is_feature:
+            z_all = x
+        else:
+            x           = x.contiguous().view( self.n_way * (self.n_support + self.n_query), *x.size()[2:]) 
+            z_all       = self.feature.forward(x)
+            z_all       = z_all.view( self.n_way, self.n_support + self.n_query, -1)
+        z_support   = z_all[:, :self.n_support]
+        z_query     = z_all[:, self.n_support:]
+
+        return z_support, z_query
+
+    def correct(self, x):       
+
+        if self.__class__.__name__ == "OurNet":
+            relation_score, cosine = self.set_forward(x)
+            scores = (relation_score + cosine)/2.
+
+        else:
+            scores = self.set_forward(x)
+
+
+        y_query = np.repeat(range( self.n_way ), self.n_query )
+
+        topk_scores, topk_labels = scores.data.topk(1, 1, True, True)
+        topk_ind = topk_labels.cpu().numpy()
+        top1_correct = np.sum(topk_ind[:,0] == y_query)
+        return float(top1_correct), len(y_query)
+
+    def train_loop(self, epoch, train_loader, optimizer ):
+        print_freq = 10
+
+        avg_loss=0
+        for i, (x,_ ) in enumerate(train_loader):
+            self.n_query = x.size(1) - self.n_support           
+            if self.change_way:
+                self.n_way  = x.size(0)
+            optimizer.zero_grad()
+            loss = self.set_forward_loss( x )
+            loss.backward()
+            optimizer.step()
+            avg_loss = avg_loss+loss.item()
+
+            if i % print_freq==0:
+                #print(optimizer.state_dict()['param_groups'][0]['lr'])
+                print('Epoch {:d} | Batch {:d}/{:d} | Loss {:f}'.format(epoch, i, len(train_loader), avg_loss/float(i+1)))
+
+    def test_loop(self, test_loader, record = None):
+        correct =0
+        count = 0
+        acc_all = []
+        
+        iter_num = len(test_loader) 
+        for i, (x,_) in enumerate(test_loader):
+            self.n_query = x.size(1) - self.n_support
+            if self.change_way:
+                self.n_way  = x.size(0)
+            correct_this, count_this = self.correct(x)
+            acc_all.append(correct_this/ count_this*100  )
+
+        acc_all  = np.asarray(acc_all)
+        acc_mean = np.mean(acc_all)
+        acc_std  = np.std(acc_all)
+        print('%d Test Acc = %4.2f%% +- %4.2f%%' %(iter_num,  acc_mean, 1.96* acc_std/np.sqrt(iter_num)))
+
+        return acc_mean
+
+
